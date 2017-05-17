8465b827b57a8f4c9b4891c53a03c40e6f0aa146
kanyoro-mungai <gmungai6@gmail.com>
2017-05-17 22:06:53 +0300

    Optimise the create_room and add_person functions

--- app/dojo.py
+++ app/dojo.py
@@ -5,15 +5,16 @@ from person import Person, Fellow, Staff
 
 class Dojo(object):
     def __init__(self):
-    	self.list_of_offices = []
-    	self.list_of_living_space= []
-    	self.list_of_staff = []
-    	self.list_of_fellows = []
-    	self.list_of_occupants = []
+        self.list_of_offices = []
+        self.list_of_living_space= []
+        self.list_of_staff = []
+        self.list_of_fellows = []
+        self.list_of_occupants = []
 
 
 
-    def create_room(self, room_type, room_name):
+    def create_room(self, room_name, room_type):
+        
         for room_name in room_name.split():
 
             if room_name in self.list_of_offices or room_name in self.list_of_living_space:
@@ -25,9 +26,9 @@ class Dojo(object):
                     new_o = Office_space(room_name)
                     self.list_of_offices.append(new_o)
 
-                    print('New {}_space is called {}'.format(room_type, room_name))
+                    print('New Office_space {} created succesfully '.format(room_name))
 
-                    print(self.list_of_offices)
+                   # print(self.list_of_offices)
 
 
 
@@ -39,39 +40,51 @@ class Dojo(object):
 
                     self.list_of_living_space.append(new_l)
 
-                    print(self.list_of_living_space)
+                   # print(self.list_of_living_space)
 
                 else:
                     print('Invalid Room Type Use either office or Living as type')
 
-    def add_person(self, name, p_type):
+
+    def add_person(self, name, p_type, accomodation = 'N'):
 
         if p_type == 'staff' or p_type == 'Staff':
-        	#create new staff
-        	new_staff = Staff(name)
-        	self.list_of_staff.append(new_staff)
-        	#Assign Office
-        	r_room = random.choice(self.list_of_offices)
+            #create new staff
+            new_staff = Staff(name)
+            self.list_of_staff.append(new_staff)
+            #Assign Office
+            r_room = random.choice(self.list_of_offices)
+            #r_room.occupants.append(new_staff)
 
-            	print ('New Staff named {0} created successfuly and assigned to {1}'.format(name, r_room.room_name))
+            print ('New Staff named {0} created successfuly and assigned to {1}'.format(name, r_room.room_name))
 
             
 
         elif p_type == 'fellow' or p_type == 'Fellow':
-        	new_fellow = Fellow(name)
-            print 'New Fellow named {} created successfuly'.format(name)
+            new_fellow = Fellow(name)
+            self.list_of_fellows.append(new_fellow)
+            #assign office
+            r_room = random.choice(self.list_of_offices)
+            print ('New Fellow named {0} created successfuly and assigned to office {1}'.format(name, r_room.room_name))
+
+            if accomodation == 'y':
+                 r_living = random.choice(self.list_of_living_space)
+                 print ('New Fellow named {0} has been assigned to living_Space {1}'.format(name, r_living.room_name))
 
         else:
             print 'invalid person type'
 
 
-    def assign_room(self):
-    	for room in self.list_of_offices:
-    		print (room.room_name, room.room_type, room.capacity)
+    # def assign_room(self):
+    #     for room in self.list_of_offices:
+    #         print (room.room_name, room.room_type, room.capacity)
 
-    	pass
+    #     pass
 
 
-newDojo = Dojo()
-newDojo.create_room('office', 'black blue green yellow white')
-newDojo.add_person('george', 'staff')
+# newDojo = Dojo()
+# newDojo.create_room('office', 'black blue green yellow white')
+# newDojo.create_room('living', 'Wet Dry moist')
+
+# newDojo.add_person('george', 'fellow', 'y')
+
