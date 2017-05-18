dfb416376c9539041a38068f75377ec4b5fbb90b
kanyoro-mungai <gmungai6@gmail.com>
2017-05-18 21:36:48 +0300

    Edits on class atrributes

--- app/person.py
+++ app/person.py
@@ -1,17 +1,20 @@
 class Person(object):
-	"""docstring for Person"""
-	def __init__(self, name, p_type):
-		self.name = name
-		self.p_type = p_type	
+    """docstring for Person"""
+
+    def __init__(self, name, p_type):
+        self.name = name
+        self.p_type = p_type
+
 
 class Fellow(Person):
-	"""docstring for Fellow"""
-	def __init__(self, name, p_type = "fellow", accomodation = "No"):
-		super(Fellow, self).__init__(name, p_type = "fellow")
-	
-					
+    """docstring for Fellow"""
+
+    def __init__(self, name, p_type="fellow", accomodation="n"):
+        super(Fellow, self).__init__(name, p_type="fellow")
+
+
 class Staff(Person):
-	"""docstring for Staff"""
-	def __init__(self, name, p_type = "staff"):
-		super (Staff, self).__init__(name, p_type = "staff")
-		
+    """docstring for Staff"""
+
+    def __init__(self, name, p_type="staff"):
+        super(Staff, self).__init__(name, p_type="staff")
--- app/room.py
+++ app/room.py
@@ -1,28 +1,22 @@
 class Room(object):
-	"""docstring for Room"""
-	def __init__(self,room_name, room_type, capacity):
-		self.room_name = room_name
-		self.room_type  = room_type
-		self.capacity = capacity
-		self.occupants = []
+    """docstring for Room"""
 
-	
-		
+    def __init__(self, room_name, room_type):
+        self.room_name = room_name
+        self.room_type = room_type
+
+
 class Office_space(Room):
-	"""docstring for Office_space"""
-	def __init__(self, room_name):
-		super(Office_space, self).__init__(room_name, room_type = 'office', capacity = 6)
+    capacity = 6
 
-		
+    def __init__(self, room_name, room_type):
+        super(Office_space, self).__init__(room_name, room_type)
+        self.room_name = room_name
 
-		
+
 class Living_quaters(Room):
-	"""docstring for Living_quaters"""
-	def __init__(self, room_name):
-		super(Living_quaters, self).__init__(room_name, room_type = "living Quaters", capacity = 4 )
+    capacity = 4
 
-
-		
-		
-			
-			
+    def __init__(self, room_name, room_type):
+        super(Living_quaters, self).__init__(room_name, room_type)
+        self.room_name = room_type
