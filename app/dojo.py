392b39e0e625f9c0c63efdcb4a4c24de23f8a648
kanyoro-mungai <gmungai6@gmail.com>
2017-05-18 21:29:31 +0300

    Fix input bug and added check for accomodation when creating staff

--- app/dojo.py
+++ app/dojo.py
@@ -1,59 +1,101 @@
 import random
-from room import Room, Office_space, Living_quaters
-from person import Person, Fellow, Staff
+from room import Office_space, Living_quaters
+from person import Fellow, Staff
 
 
 class Dojo(object):
 
     def __init__(self):
-        self.list_of_offices = []
-        self.list_of_living_space = []
+        self.unallocated = []
+        self.list_of_offices = {}
+        self.list_of_living_space = {}
         self.list_of_staff = []
         self.list_of_fellows = []
-        self.list_of_occupants = []
 
     def create_room(self, room_type, room_name):
-
+        room_type = room_type.lower()
         if room_name not in self.list_of_offices and room_name not in self.list_of_living_space:
-            if room_type == 'office' or room_type == 'Office':
-            # create a new office
-              new_o = Office_space(room_name)
-              self.list_of_offices.append(room_name)
-              print('New Office_space {} created succesfully '.format(room_name))
 
-            elif room_type == 'living' or room_type == 'Living':
-             # create a new living
-                new_l = Living_quaters(room_name)
-                print ('New {}_space is called {}'.format(room_type, room_name))
-                self.list_of_living_space.append(room_name)
+            if room_type == 'office':
+                # create a new office
+                new_office = Office_space(room_name, room_type)
+                self.list_of_offices[room_name] = []
+                print('\n')
+                print('______________Office Created __________')
+                print('New Office {} created succesfully'.format(room_name))
+
+            elif room_type == 'living':
+                # create a new living
+                new_l = Living_quaters(room_name, room_type)
+                self.list_of_living_space[room_name] = []
+                print('\n')
+                print('______________Living Space Created __________')
+                print('New Living_space is called {}'.format(room_name))
 
             else:
                 print('Invalid Room Type Use either office or Living as type')
 
-            print(self.list_of_offices)
         else:
             print("sorry... room {} already exists").format(room_name)
 
+    def allocate_office_space(self, name):
+        try:
+            random_room = random.choice(list(self.list_of_offices))
+            if(len(self.list_of_offices[random_room])) < Office_space.capacity:
+                self.list_of_offices[random_room].append(name)
+                print('\n')
+                print('______________Office Space Allocation__________')
+                print('{} has been assigned to {} office space'.format(
+                    name, random_room))
+            else:
+                self.allocate_office_space(name)
+        except:
+            self.unallocated.append(name)
+
+    def allocate_living_space(self, name):
+        try:
+            random_room = random.choice(list(self.list_of_living_space))
+            if(len(self.list_of_living_space[random_room])) < Living_quaters.capacity:
+                self.list_of_living_space[random_room].append(name)
+                print('\n')
+                print('______________Living Space Allocation__________')
+                print('{} has been assigned to {} living space'.format(
+                    name, random_room))
+            else:
+                self.allocate_living_space(name)
+        except:
+            self.unallocated.append(name)
 
     def add_person(self, name, p_type, accomodation='N'):
-         if p_type == 'staff' or p_type == 'Staff':
+        p_type = p_type.lower()
+        accomodation = accomodation.lower()
+
+        if p_type == 'staff':
             # create new staff
-            new_staff = Staff(name)
+            new_staff = Staff(name, accomodation)
             self.list_of_staff.append(name)
-                # Assign Office
-            r_room = random.choice(self.list_of_offices)
-            print('New Staff named {} created successfuly and assigned to {}'.format(name, r_room))
-         elif p_type == 'fellow' or p_type == 'Fellow':
-            #create new fellow
+            print('\n')
+            print('______________STAFF__________')
+            print('New Staff named {} created successfuly'.format(name))
+            self.allocate_office_space(name)
+
+            if (accomodation == 'y'):
+                print('\n')
+                print('______________Living Space Allocation__________')
+                print('Sorry Living space not available for staff')
+
+        elif p_type == 'fellow':
+            # create new fellow
             new_fellow = Fellow(name)
             self.list_of_fellows.append(name)
-             # assign office
-            r_room = random.choice(self.list_of_offices)
-            print('New Fellow named {0} created successfuly and assigned to office {1}'.format(name, r_room))
+            print('\n')
+            print('______________FELLOW__________')
+            print('New Fellow named {} created'.format(name))
+            print('\n')
+
+            self.allocate_office_space(name)
 
             if accomodation == 'y':
-                r_living = random.choice(self.list_of_living_space)
-                print('New Fellow named {0} has been assigned to living_Space {1}'.format(name, r_living))
-         else:
-            print ('invalid person type')
-
+                self.allocate_living_space(name)
+        else:
+            print('invalid person type')
